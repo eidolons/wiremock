93092003ade7681f8e1bf75e6f9e70b0d587be7c
Toshiaki Ezaki <toshiaki.ezaki@eidolons.io>
2020-10-12 02:20:12 -0300

    Added logging configuration and also fixed problems with double quotes.

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,7 @@
 FROM eidolons/openjdk:8.0.265
 
 # Container environment variables
-ENV WIREMOCK_HOME="/opt/wiremock" \
-    WIREMOCK_SECURED="false" \
+ENV WIREMOCK_SECURED="false" \
     WIREMOCK_CASTORE="application.castore" \
     WIREMOCK_CASTORE_PASSWORD="password" \
     WIREMOCK_KEYSTORE="application.keystore" \
@@ -11,10 +10,14 @@ ENV WIREMOCK_HOME="/opt/wiremock" \
     WIREMOCK_TRUSTSTORE="application.truststore" \
     WIREMOCK_TRUSTSTORE_PASSWORD="password" \
     WIREMOCK_RECORD_MAPPINGS="false" \
-    WIREMOCK_REQUEST_JOURNAL="false" \
+    WIREMOCK_REQUEST_JOURNAL="true" \
     WIREMOCK_BROWSER_PROXING="false" \
-    WIREMOCK_VERBOSE="false" \
-    WIREMOCK_VERSION="2.27.0"
+    WIREMOCK_VERBOSE="false"
+
+ARG WIREMOCK_HOME="/opt/wiremock"
+ARG WIREMOCK_VERSION="2.27.0"
+ARG SLF4J_VERSION="1.7.30"
+ARG LOG4J_VERSION="2.13.3"
 
 # Update 'openssl' package
 # Grab su-exec for easy step-down from root and bash
@@ -22,7 +25,19 @@ RUN apk add --update openssl && \
     apk add --no-cache 'su-exec>=0.2' bash
 
 # Grab wiremock standalone jar
-RUN mkdir -p /opt/wiremock/conf && mkdir -p /opt/wiremock/data && mkdir -p /var/wiremock/lib && mkdir -p /var/wiremock/extensions \
+RUN mkdir -p /opt/wiremock/conf && mkdir -p /opt/wiremock/data && mkdir -p /var/wiremock/conf && mkdir -p /var/wiremock/lib && mkdir -p /var/wiremock/extensions \
+  && wget https://repo1.maven.org/maven2/org/slf4j/slf4j-api/${SLF4J_VERSION}/slf4j-api-${SLF4J_VERSION}.jar \
+    -O /var/wiremock/lib/slf4j-api.jar \
+  && chmod 644 /var/wiremock/lib/slf4j-api.jar \
+  && wget https://repo1.maven.org/maven2/org/apache/logging/log4j/log4j-slf4j-impl/${LOG4J_VERSION}/log4j-slf4j-impl-${LOG4J_VERSION}.jar \
+    -O /var/wiremock/lib/log4j-slf4j-impl.jar \
+  && chmod 644 /var/wiremock/lib/log4j-slf4j-impl.jar \
+  && wget https://repo1.maven.org/maven2/org/apache/logging/log4j/log4j-api/${LOG4J_VERSION}/log4j-api-${LOG4J_VERSION}.jar \
+    -O /var/wiremock/lib/log4j-api.jar \
+  && chmod 644 /var/wiremock/lib/log4j-api.jar \
+  && wget https://repo1.maven.org/maven2/org/apache/logging/log4j/log4j-core/${LOG4J_VERSION}/log4j-core-${LOG4J_VERSION}.jar \
+    -O /var/wiremock/lib/log4j-core.jar \
+  && chmod 644 /var/wiremock/lib/log4j-core.jar \
   && wget https://repo1.maven.org/maven2/com/github/tomakehurst/wiremock-jre8-standalone/${WIREMOCK_VERSION}/wiremock-jre8-standalone-${WIREMOCK_VERSION}.jar \
     -O /var/wiremock/lib/wiremock-jre8-standalone.jar \
   && chmod 644 /var/wiremock/lib/wiremock-jre8-standalone.jar
@@ -33,8 +48,10 @@ RUN mkdir -p /opt/wiremock/conf && mkdir -p /opt/wiremock/data && mkdir -p /var/
 RUN addgroup -g 1000 -S wiremock && adduser -u 1000 -D -h /opt/wiremock -s /sbin/nologin -S wiremock -G wiremock && chmod 755 -R /opt/wiremock
 
 # Copy files to container
-COPY docker-entrypoint.sh /
-RUN chmod 755 /docker-entrypoint.sh && \
+COPY assets/log4j2.xml /var/wiremock/conf
+COPY assets/docker-entrypoint.sh /
+RUN chmod 755 "/docker-entrypoint.sh" && \
+    chmod 644 "/var/wiremock/conf/log4j2.xml" && \
     chown -R wiremock:wiremock /opt/wiremock
 
 # Set the working directory to wiremock's user home directory
--- assets/docker-entrypoint.sh
+++ assets/docker-entrypoint.sh
@@ -4,30 +4,31 @@ set -e
 
 # Add `java -jar wiremock-jre8-standalone.jar` as command if needed
 if [ "${1}" = "" ]; then
-    WIREMOCK_OPTS="--root-dir \"${WIREMOCK_HOME}/data\""
+    WIREMOCK_HOME="/opt/wiremock"
+    WIREMOCK_OPTS="--root-dir ${WIREMOCK_HOME}/data"
     # Check whether to enable HTTPS or use HTTP.
     if [ "${WIREMOCK_SECURED}" = "true" ]; then
         WIREMOCK_OPTS="${WIREMOCK_OPTS} --disable-http --https-port 8443"
 
         # Check the certificate authority keystore configuration.
-        if [ ! "${WIREMOCK_CASTORE}" = "" ] && [ -f "${WIREMOCK_HOME}/${WIREMOCK_CASTORE}" ]; then
-            WIREMOCK_OPTS="${WIREMOCK_OPTS} --ca-keystore \"${WIREMOCK_HOME}/${WIREMOCK_CASTORE}\""
+        if [ ! "${WIREMOCK_CASTORE}" = "" ] && [ -f "${WIREMOCK_HOME}/conf/${WIREMOCK_CASTORE}" ]; then
+            WIREMOCK_OPTS="${WIREMOCK_OPTS} --ca-keystore ${WIREMOCK_HOME}/conf/${WIREMOCK_CASTORE}"
             if [ ! "${WIREMOCK_TRUSTSTORE_PASSWORD}" = "" ]; then
-                WIREMOCK_OPTS="${WIREMOCK_OPTS} --ca-keystore-type pkcs12 --ca-keystore-password \"${WIREMOCK_CASTORE_PASSWORD}\""
+                WIREMOCK_OPTS="${WIREMOCK_OPTS} --ca-keystore-type pkcs12 --ca-keystore-password ${WIREMOCK_CASTORE_PASSWORD}"
             fi
         fi
         # Check the keystore configuration.
-        if [ ! "${WIREMOCK_KEYSTORE}" = "" ] && [ -f "${WIREMOCK_HOME}/${WIREMOCK_KEYSTORE}" ]; then
-            WIREMOCK_OPTS="${WIREMOCK_OPTS} --https-keystore \"${WIREMOCK_HOME}/${WIREMOCK_KEYSTORE}\""
+        if [ ! "${WIREMOCK_KEYSTORE}" = "" ] && [ -f "${WIREMOCK_HOME}/conf/${WIREMOCK_KEYSTORE}" ]; then
+            WIREMOCK_OPTS="${WIREMOCK_OPTS} --https-keystore ${WIREMOCK_HOME}/conf/${WIREMOCK_KEYSTORE}"
             if [ ! "${WIREMOCK_KEYSTORE_PASSWORD}" = "" ]; then
-                WIREMOCK_OPTS="${WIREMOCK_OPTS} --keystore-type pkcs12 --keystore-password \"${WIREMOCK_KEYSTORE_PASSWORD}\" --key-manager-pasword \"${WIREMOCK_KEYSTORE_PASSWORD}\""
+                WIREMOCK_OPTS="${WIREMOCK_OPTS} --keystore-type pkcs12 --keystore-password ${WIREMOCK_KEYSTORE_PASSWORD}"
             fi
         fi
         # Check the truststore configuration.
-        if [ ! "${WIREMOCK_TRUSTSTORE}" = "" ] && [ -f "${WIREMOCK_HOME}/${WIREMOCK_TRUSTSTORE}" ]; then
-            WIREMOCK_OPTS="${WIREMOCK_OPTS} --disable-http --https-port 8443 --https-truststore \"${WIREMOCK_HOME}/${WIREMOCK_TRUSTSTORE}\""
+        if [ ! "${WIREMOCK_TRUSTSTORE}" = "" ] && [ -f "${WIREMOCK_HOME}/conf/${WIREMOCK_TRUSTSTORE}" ]; then
+            WIREMOCK_OPTS="${WIREMOCK_OPTS} --disable-http --https-port 8443 --https-truststore ${WIREMOCK_HOME}/conf/${WIREMOCK_TRUSTSTORE}"
             if [ ! "${WIREMOCK_TRUSTSTORE_PASSWORD}" = "" ]; then
-                WIREMOCK_OPTS="${WIREMOCK_OPTS} --truststore-type pkcs12 --truststore-password \"${WIREMOCK_TRUSTSTORE_PASSWORD}\""
+                WIREMOCK_OPTS="${WIREMOCK_OPTS} --truststore-type pkcs12 --truststore-password ${WIREMOCK_TRUSTSTORE_PASSWORD}"
             fi
         fi
     else
@@ -46,6 +47,11 @@ if [ "${1}" = "" ]; then
     # Check if you want to enable verbose logging.
     if [ "${WIREMOCK_VERBOSE}" = "true" ]; then
         WIREMOCK_OPTS="${WIREMOCK_OPTS} --verbose"
+    fi
+    if [ -f "${WIREMOCK_HOME}/conf/log4j2.xml" ]; then
+        JAVA_OPTS="${JAVA_OPTS} -Dlog4j.configurationFile=${WIREMOCK_HOME}/conf/log4j2.xml"
+    else
+        JAVA_OPTS="${JAVA_OPTS} -Dlog4j.configurationFile=/var/wiremock/conf/log4j2.xml"
     fi
     # Default command line configuration.
     set -- java ${JAVA_OPTS} -cp /var/wiremock/lib/*:/var/wiremock/extensions/* com.github.tomakehurst.wiremock.standalone.WireMockServerRunner ${WIREMOCK_OPTS} "${@}"
